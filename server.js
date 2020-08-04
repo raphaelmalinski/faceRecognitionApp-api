372be733a7e01472f655c6c9d1e2e5bdf0a18997
Raphael <raphaelmalinski@gmail.com>
2020-08-03 22:24:33 -0300

    Creating server and add signin and register funcionality

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'Raphael',
+            email: 'raphael@gmail.com',
+            password: 'bafenta',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name: 'Vitoria',
+            email: 'vitoria@gmail.com',
+            password: 'nego',
+            entries: 0,
+            joined: new Date()
+        }
+    ]
+}
+
+app.get('/', (req, res) => {
+    res.send(database.users);
+});
+
+app.post('/signin', (req, res) => {
+    if (req.body.email === database.users[0].email &&
+        req.body.password === database.users[0].password) {
+        res.json('success');
+    } else {
+        res.status(400).json('error logging in');
+    }
+});
+
+app.post('/register', (req, res) => {
+    const { email, name, password } = req.body;
+    database.users.push({
+        id: '125',
+        name: 'Maria',
+        email: 'maria@gmail.com',
+        password: 'veia',
+        entries: 0,
+        joined: new Date()
+    });
+    res.json(database.users[database.users.length - 1]);
+});
+
+app.listen(3000, () => {
+    console.log('app is running on port 3000');
+});
