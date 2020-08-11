e3f4c9b57358cbc95476e586535d46bc03efcecb
Raphael <raphaelmalinski@gmail.com>
2020-08-10 22:05:36 -0300

    Updating register functionality to run with database

--- server.js
+++ server.js
@@ -90,14 +90,32 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
     const { email, name, password } = req.body;
-    database.users.push({
-        id: '125',
-        name: name,
-        email: email,
-        entries: 0,
-        joined: new Date()
-    });
-    res.json(database.users[database.users.length - 1]);
+    const hash = bcrypt.hashSync(password);
+    db.transaction(trx => {
+        trx.insert({
+            hash: hash,
+            email: email
+        })
+            .into('login')
+            .returning('email')
+            .then(loginEmail => {
+                return trx('users')
+                    .returning('*')
+                    .insert({
+                        email: loginEmail[0],
+                        name: name,
+                        joined: new Date()
+                    })
+                    .then(user => {
+                        res.json(user[0]);
+                    })
+                    .catch(err => {
+                        res.status(400).json('unable to register');
+                    });
+            })
+            .then(trx.commit)
+            .catch(trx.rollback);
+    })
 });
 
 app.get('/profile/:id', (req, res) => {
